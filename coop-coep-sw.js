b1cb479977b34422fcb6d34843f9ed9a4dabc4c3
Zexi Yin <jersey1dev@gmail.com>
2025-11-05 11:42:47 +0800

    github deployment works

--- coop-coep-sw.js
+++ coop-coep-sw.js
@@ -7,25 +7,31 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      (async () => {
-        try {
-          const response = await fetch(event.request);
-          const headers = new Headers(response.headers);
-          headers.set('Cross-Origin-Opener-Policy', 'same-origin');
-          headers.set('Cross-Origin-Embedder-Policy', 'require-corp');
+  const { request } = event;
 
-          return new Response(response.body, {
-            status: response.status,
-            statusText: response.statusText,
-            headers: headers,
-          });
-        } catch (e) {
-          console.error('Service worker fetch failed:', e);
-          return e;
-        }
-      })()
-    );
+  // If it's not a request for our origin, let it pass through.
+  if (request.url.startsWith('http') && !request.url.startsWith(self.location.origin)) {
+    return;
   }
+
+  // For same-origin requests, fetch and add the headers.
+  event.respondWith(
+    (async () => {
+      try {
+        const response = await fetch(request);
+        const headers = new Headers(response.headers);
+        headers.set('Cross-Origin-Opener-Policy', 'same-origin');
+        headers.set('Cross-Origin-Embedder-Policy', 'require-corp');
+
+        return new Response(response.body, {
+          status: response.status,
+          statusText: response.statusText,
+          headers: headers,
+        });
+      } catch (e) {
+        console.error('Service worker fetch failed:', e);
+        return e;
+      }
+    })()
+  );
 });
